a9762f4e9a59216665ecc791bbd71d5552add255
Sybren A. Stüvel <sybren@stuvel.eu>
2019-11-12 14:20:16 +0100

    Updated NPM dependencies, including Gulp 3.9 → 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
-var argv         = require('minimist')(process.argv.slice(2));
-var gulp         = require('gulp');
-var plumber      = require('gulp-plumber');
-var pug          = require('gulp-pug');
-var uglify       = require('gulp-uglify').default;
+const gulp    = require('gulp');
+const plumber = require('gulp-plumber');
+const pug     = require('gulp-pug');
 
 /* Templates - Pug */
-gulp.task('templates', function() {
+gulp.task('templates', function(done) {
     gulp.src('src/templates/**/*.pug')
         .pipe(plumber())
         .pipe(pug({
             pretty: false
         }))
         .pipe(gulp.dest('templates'));
+    done();
 });
 
 // While developing, run 'gulp watch'
 gulp.task('watch',function() {
-    gulp.watch('src/templates/**/*.pug',['templates']);
+    gulp.watch('src/templates/**/*.pug', gulp.series(['default']));
 });
 
 // Run 'gulp' to build everything at once
-gulp.task('default', ['templates']);
+gulp.task('default', gulp.series(['templates']));
